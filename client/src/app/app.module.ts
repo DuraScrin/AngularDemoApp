d6bf4c5608800b455ee8bf828427359c5b011869
Dimitri Mechenko <dimitri.mechenko@syntra-ab.be>
2022-09-02 15:54:44 +0200

    First angular service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import {HttpClientModule} from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {HttpClientModule} from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,8 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
 
-  constructor() { }
+export class NavComponent implements OnInit 
+{
+  constructor(private accountService: AccountService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  model: any = {}
+  loggedIn: boolean;
+  login()
+  {
+    this.accountService.login(this.model).subscribe(response =>
+      {
+        console.log(response);
+        this.loggedIn = true;
+      }, error =>
+      {
+        console.log(error);
+      });
   }
 
+  logout()
+  {
+    this.loggedIn = false;
+  }
 }
